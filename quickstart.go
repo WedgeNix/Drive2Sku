9d0409610eff621a177b9a4e985f89dbb01972a2
PǝRV <alexander.w.matulionis@gmail.com>
2017-04-06 14:41:17 -0700

    Walk the JSON file

--- quickstart.go
+++ quickstart.go
@@ -7,6 +7,8 @@ import (
 
 	"net/http"
 
+	"encoding/json"
+
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v3"
@@ -57,9 +59,10 @@ func drive2Sku() {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
 
+	// obtain our Google Drive and SKUVault handles
 	drv, sku = getClientAndSkuTokens(context.Background(), config)
+
 	readDrive()
-	write2Sku()
 }
 
 // readDrive actually reads the drive account's
@@ -79,17 +82,18 @@ func readDrive() {
 			fmt.Printf("%s (%s)\n", f.Name, f.Id)
 
 			// grabs http request for one of the json files
-			// r, err := d2s.service.Files.Get(f.Id).Download()
+			// res, err := drv.Files.Get(f.Id).Download()
 			// if err != nil {
 			// 	log.Fatalf("Unable to download file: %v", err)
 			// }
 
 			// goroutine forwards json (request) to sku database
-			// go write2Sku(r)
+			// go write2Sku(res)
 		}
 	} else {
 		fmt.Println("No files found.")
 	}
+	write2Sku(nil)
 }
 
 // SkuConn contains access tokens for POST calls
@@ -101,9 +105,75 @@ type SkuConn struct {
 	c http.Client
 }
 
+// SkuData represents the inner, important information for each sku object
+// this exists in the JSON structure
+//
+type SkuData struct {
+	Sku         string
+	Quantity    int
+	Location    string
+	WarehouseID string
+}
+
 // write2Sku writes the intercepted json files out
 // to SKUVault via its REST api
 //
-func write2Sku() {
+func write2Sku(res *http.Response) {
 	fmt.Printf("Tenant:%s User:%s\n", sku.t.TenantToken, sku.t.UserToken)
+
+	const jStream = `
+		{"Famous":{"0ABC123X":{"Sku":"0ABC123X","Quantity":6,"Location":"Upland","WarehouseId":"W2"},"0ABC123Y":{"Sku":"0ABC123Y","Quantity":7,"Location":"Upland","WarehouseId":"W2"}}}
+	`
+
+	vsd := map[string]map[string]SkuData{}
+	json.Unmarshal([]byte(jStream), &vsd)
+	for k, v := range vsd {
+		fmt.Printf("%s:\n", k)
+		for ik, iv := range v {
+			fmt.Printf("\t%s:\n", ik)
+			fmt.Printf("\t\t\"Sku\":\"%s\"\n", iv.Sku)
+			fmt.Printf("\t\t\"Quantity\":\"%d\"\n", iv.Quantity)
+			fmt.Printf("\t\t\"Location\":\"%s\"\n", iv.Location)
+			fmt.Printf("\t\t\"WarehouseId\":\"%s\"\n", iv.WarehouseID)
+		}
+	}
+
+	// body := json.NewDecoder(res.Body)
+	// body := json.NewDecoder(strings.NewReader(jStream))
+	// for {
+	// 	tok, err := body.Token()
+	// 	if err == io.EOF {
+	// 		break
+	// 	}
+	// 	if err != nil {
+	// 		log.Fatalf("Unable to tokenize vendor json: %v", err)
+	// 	}
+
+	// 	fmt.Printf("%T: %v", tok, tok)
+
+	// 	if body.More() {
+	// 		fmt.Printf(" (more)")
+	// 	}
+
+	// 	fmt.Println()
+	// }
+
+	// skuRequest("setQuantities")
 }
+
+// func skuRequest(fn string) {
+// 	req, err := http.NewRequest("POST", "https://app.skuvault.com/api/"+fn)
+// 	if err != nil {
+// 		log.Fatalf("Unable to obtain SKUVault request: %v", err)
+// 	}
+// 	req.Header.Add("accept", "application/json")
+// 	req.Header.Add("content-type", "application/json")
+
+// 	// http client based on request initialized
+// 	client := &http.Client{}
+// 	res, err := client.Do(req)
+// 	if err != nil {
+// 		log.Fatalf("Unable to get SKUVault response: %v", err)
+// 	}
+// 	defer res.Body.Close()
+// }
