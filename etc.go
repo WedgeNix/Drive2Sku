df62d8d781e0ce6cf53055226f19e067ccffdf3d
PǝRV <alexander.w.matulionis@gmail.com>
2017-04-13 17:03:23 -0700

    It did its job

--- etc.go
+++ etc.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
@@ -13,8 +14,6 @@ import (
 
 	"google.golang.org/api/drive/v3"
 
-	"time"
-
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 )
@@ -22,7 +21,7 @@ import (
 // getClientAndSkuTokens uses a Context and Config to retrieve a Token
 // then generate a Client. It returns the generated Client.
 //
-func getClientAndSkuTokens(ctx context.Context, config *oauth2.Config) (*drive.Service, *SkuConn) {
+func getClientAndSkuTokens(ctx context.Context, config *oauth2.Config) (*drive.Service, *SkuTokens) {
 	cacheDriveFile, cacheSkuFile, err := tokenCacheFiles()
 	if err != nil {
 		log.Fatalf("Unable to get path to cached credential files. %v", err)
@@ -36,10 +35,10 @@ func getClientAndSkuTokens(ctx context.Context, config *oauth2.Config) (*drive.S
 	}
 
 	// skuvault token
-	sku, err := tokenFromFile(cacheSkuFile)
+	toks, err := tokensFromFile(cacheSkuFile)
 	if err != nil {
-		sku = getTokenFromWeb()
-		saveToken(cacheSkuFile, sku.t)
+		toks = getTokensFromWeb()
+		saveTokens(cacheSkuFile, toks)
 	}
 
 	drv, err = drive.New(config.Client(ctx, tok))
@@ -47,7 +46,7 @@ func getClientAndSkuTokens(ctx context.Context, config *oauth2.Config) (*drive.S
 		log.Fatalf("Unable to retrieve drive Service: %v", err)
 	}
 
-	return drv, sku
+	return drv, toks
 }
 
 // getOTokenFromWeb uses Config to request a Token.
@@ -106,10 +105,10 @@ type SkuTokens struct {
 	UserToken   string
 }
 
-// tokenFromFile retrieves a Token from a given file path.
+// tokensFromFile retrieves a Token from a given file path.
 // It returns the retrieved Token and any read error encountered.
 //
-func tokenFromFile(file string) (*SkuConn, error) {
+func tokensFromFile(file string) (*SkuTokens, error) {
 	f, err := os.Open(file)
 	if err != nil {
 		return nil, err
@@ -117,7 +116,7 @@ func tokenFromFile(file string) (*SkuConn, error) {
 	t := &SkuTokens{}
 	err = json.NewDecoder(f).Decode(t)
 	defer f.Close()
-	return &SkuConn{*t, http.Client{}}, err
+	return t, err
 }
 
 // saveOToken uses a file path to create a file and store the
@@ -133,23 +132,54 @@ func saveOToken(file string, token *oauth2.Token) {
 	json.NewEncoder(f).Encode(token)
 }
 
-// saveToken uses a file path to create a file and store the
+// saveTokens uses a file path to create a file and store the
 // token in it.
 //
-func saveToken(file string, token SkuTokens) {
+func saveTokens(file string, toks *SkuTokens) {
 	fmt.Printf("Saving SkuVault credential file to: %s\n", file)
 	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
 		log.Fatalf("Unable to cache sku tokens: %v", err)
 	}
 	defer f.Close()
-	json.NewEncoder(f).Encode(token)
+	json.NewEncoder(f).Encode(toks)
+}
+
+// struct2JSON converts a data structure
+// in type format into a JSON-reader
+//
+func struct2JSON(v interface{}) *strings.Reader {
+	// convert interface to JSON bytes
+	b, err := json.Marshal(v)
+	if err != nil {
+		log.Fatalf("Unable to convert interface to reader: %v", err)
+	}
+
+	// return reader of stringified JSON bytes
+	return strings.NewReader(string(b))
+}
+
+// vaultRequest asks SKUVault of the passed in function,
+// supplying a reader on a JSON string
+//
+func vaultRequest(fn string, jsn *strings.Reader) (*http.Response, error) {
+	// get official POST request from SKUVault
+	req, err := http.NewRequest("POST", "https://app.skuvault.com/api/"+fn, jsn)
+	if err != nil {
+		log.Fatalf("Unable to obtain SKUVault request: %v", err)
+	}
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("content-type", "application/json")
+
+	// http client based on request initialized
+	client := &http.Client{}
+	return client.Do(req)
 }
 
 // getSkuCredentials gets the tokens needed for SKU vault
 // api calls.
 //
-func getTokenFromWeb() *SkuConn {
+func getTokensFromWeb() *SkuTokens {
 	//  Asking for email for SKU Vault account
 	// fmt.Printf("SKU Vault email and password: ")
 	// fmt.Printf("Enter your SKU Valut Email address.\n")
@@ -192,19 +222,9 @@ func getTokenFromWeb() *SkuConn {
 		log.Fatalf("Unable to decode skuvault-acc.json: %v", err)
 	}
 
-	// get official POST request from SKUVault
-	req, err := http.NewRequest("POST", "https://app.skuvault.com/api/getTokens", struct2JSON(lgn))
+	res, err := vaultRequest(`getTokens`, struct2JSON(lgn))
 	if err != nil {
-		log.Fatalf("Unable to obtain SKUVault request: %v", err)
-	}
-	req.Header.Add("accept", "application/json")
-	req.Header.Add("content-type", "application/json")
-
-	// http client based on request initialized
-	client := &http.Client{}
-	res, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("Unable to get SKUVault response: %v", err)
+		log.Fatalf("Unable to get SKUVault tokens: %v", err)
 	}
 	defer res.Body.Close()
 
@@ -212,40 +232,16 @@ func getTokenFromWeb() *SkuConn {
 	toks := &SkuTokens{}
 	err = json.NewDecoder(res.Body).Decode(toks)
 	if err != nil {
-		log.Fatalf("Unable to decode SKUVault tokens: %v", err)
+		log.Fatalf("Unable to decode SKUVault tokens {\n\t%s,\n\t%s\n}\n: %v", toks.TenantToken, toks.UserToken, err)
 	}
 
-	return &SkuConn{*toks, *client}
+	return toks
 }
 
-// struct2JSON converts a data structure
-// in type format into a JSON-reader
-//
-func struct2JSON(v interface{}) *strings.Reader {
-	// convert interface to JSON bytes
-	b, err := json.Marshal(v)
+func printResponse(res *http.Response) {
+	b, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Fatalf("Unable to convert interface to reader: %v", err)
+		log.Fatalf(`Unable to read SKUVault response body: %v`, err)
 	}
-
-	// return reader of stringified JSON bytes
-	return strings.NewReader(string(b))
-}
-
-func weekdaysOperations() {
-	weekly := time.Tick(7 * 24 * time.Hour)
-	// throttle := time.Tick(1 * time.Minute)
-	// rddrv := time.Tick(15 * time.Minute)
-
-	for {
-	wknd: // resets once the weekend starts
-		select {
-		case <-weekly:
-			weekend := time.After(5 * 24 * time.Hour)
-			select {
-			case <-weekend:
-				break wknd
-			}
-		}
-	}
+	fmt.Println(string(b))
 }
--- main.go
+++ main.go
@@ -4,126 +4,16 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-
-	"net/http"
+	"time"
 
 	"encoding/json"
 
-	"time"
+	"sync"
 
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v3"
 )
-
-const (
-	pendingVendors = 20
-)
-
-var (
-	// drv is the Google Drive service
-	// it references the account after connecting
-	//
-	drv *drive.Service
-
-	// sku is the SKUVault connection tokens and client
-	// it allows use of tenant and user tokens for POST calls
-	//
-	sku *SkuConn
-)
-
-// main is the entry point into the server program
-// first sets up and reads from the drive
-// then forwards the json files in their proper
-// format out to SKUVault.
-// It loops, controlling the flow, timing, and efficiency
-// of the server program so it runs on schedule
-// in a smart and practical manner
-//
-func main() {
-	initDriveAndVault()
-
-	readDriveChannel := time.Tick(15 * time.Minute)
-	fileChannelBuffer := make(chan drive.File, 20)
-	writeVaultChannel := time.Tick(1 * time.Minute)
-
-	for {
-		select {
-		case <-readDriveChannel:
-			readDrive()
-		case <-writeVaultChannel:
-			writeVault()
-		}
-	}
-
-	// for {
-	// drive2Sku()
-
-	// TODO: uncomment line below when the program is
-	// | | | ready to run on own
-	// v v v
-	// time.Sleep(24 * time.Hour)
-	// }
-}
-
-func readDrive() {
-	// initially handle 10 vendors concurrently
-	fch := make(chan drive.File, 10)
-	readPendingVendors(fch)
-	write2SkuVault(fch)
-}
-
-// init creates an instance of the engine's collective data
-// it sets up the dialog between this server and the drive folder
-//
-func initDriveAndVault() {
-	b, err := ioutil.ReadFile("client_secret.json")
-	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
-	}
-
-	// If modifying these scopes, delete your previously saved credentials
-	// at ~/.credentials/drive-go-quickstart.json
-	config, err := google.ConfigFromJSON(b, drive.DriveScope)
-	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
-	}
-
-	// obtain our Google Drive and SKUVault handles
-	drv, sku = getClientAndSkuTokens(context.Background(), config)
-}
-
-// readPendingVendors actually reads the drive account's
-// pending vendors folder and grabs any and all
-// files, downloads them, and deletes them
-//
-func readPendingVendors(fch chan drive.File) {
-	// all Pending Vendor parent id files not in the trash
-	fls, err := drv.Files.List().Q("'0BzaYO4E7QW9VNG5GejI1LUExaGM' in parents and trashed = false").Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve files: %v", err)
-	}
-
-	if len(fls.Files) > 0 {
-		for _, f := range fls.Files {
-			fmt.Printf("[[[ Processing %s (%s) ]]]\n", f.Name, f.Id)
-
-			// pass file handle into channel
-			fch <- *f
-		}
-	} else {
-		fmt.Println("No files found.")
-	}
-}
-
-// SkuConn contains access tokens for POST calls
-// t represents the POST tenant and user tokens
-// c represents the POST http client
-//
-type SkuConn struct {
-	t SkuTokens
-	c http.Client
-}
 
 // Item represents the inner, important information for each sku object
 // this exists in the JSON structure
@@ -144,81 +34,210 @@ type Payload struct {
 	UserToken   string
 }
 
-// write2SkuVault writes the intercepted json files out
-// to SKUVault via its REST api
+const (
+	// throttle is SKUVault's throttle limit
+	// ten 100-object payloads every minute
+	// every 6300 milliseconds, a post is made
+	//
+	throttle = 6300
+)
+
+var (
+	// drv is the Google Drive service
+	// it references the account after connecting
+	//
+	drv *drive.Service
+
+	// toks is the SKUVault connection tokens and client
+	// it allows use of tenant and user tokens for POST calls
+	//
+	toks *SkuTokens
+
+	// endCh signifies the end of the program
+	// it is done processing everything once the last
+	// value is passed through it
+	//
+	endCh = make(chan bool)
+
+	// plBufCh holds a maximum of 10 payloads stored concurrently
+	//
+	plBufCh = make(chan Payload, 10)
+
+	wg sync.WaitGroup
+)
+
+// main is the entry point into the server program
+// first sets up and reads from the drive
+// then forwards the json files in their proper
+// format out to SKUVault.
+// It loops, controlling the flow, timing, and efficiency
+// of the server program so it runs on schedule
+// in a smart and practical manner
 //
-func write2SkuVault(fch chan drive.File) {
-	// grabs http request for one of the json files
-	res, err := drv.Files.Get((<-fch).Id).Download()
+func main() {
+	initDriveAndVault()
+
+	wg.Add(1)
+	go readDrive()
+
+	// wait for everyone to finish their jobs
+	go proctor()
+
+	// 10 payloads every minute to SKUVault
+	throttleCh := time.Tick(throttle * time.Millisecond)
+	for {
+		select {
+		case <-throttleCh:
+			go writeVault(<-plBufCh)
+		case <-endCh:
+			fmt.Printf("[[[ Finished relaying vendor JSONs ]]]\n")
+			return
+		}
+	}
+}
+
+func proctor() {
+	wg.Wait()
+	endCh <- true
+}
+
+// init creates an instance of the engine's collective data
+// it sets up the dialog between this server and the drive folder
+//
+func initDriveAndVault() {
+	b, err := ioutil.ReadFile("client_secret.json")
 	if err != nil {
-		log.Fatalf("Unable to download file: %v", err)
+		log.Fatalf("Unable to read client secret file: %v", err)
 	}
 
-	//
-	// payloads := make([]Payload, 0, 10)
+	// If modifying these scopes, delete your previously saved credentials
+	// at ~/.credentials/drive-go-quickstart.json
+	config, err := google.ConfigFromJSON(b, drive.DriveScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
 
-	// 100-item capacity payload
-	// pyld := Payload{make([]Item, 0, 100), sku.t.TenantToken, sku.t.UserToken}
+	// obtain our Google Drive and SKUVault handles
+	drv, toks = getClientAndSkuTokens(context.Background(), config)
+}
 
-	// the entire JSON file structure
-	vsd := map[string]map[string]Item{}
-	json.NewDecoder(res.Body).Decode(&vsd)
-	for k, v := range vsd {
-		fmt.Printf("%s:\n", k)
+// readPendingVendors actually reads the drive account's
+// pending vendors folder and grabs any and all
+// files, downloads them, and deletes them
+//
+func readDrive() {
+	defer wg.Done()
 
-		// this is one payload item
-		for ik, iv := range v {
-			fmt.Printf("\t%s:\n", ik)
+	// all Pending Vendor parent id files not in the trash
+	fls, err := drv.Files.List().PageSize(1).Q(`'0BzaYO4E7QW9VeVFVUGZrMUVLSWs' in parents and trashed = false`).Do()
+	if err == nil {
+		if len(fls.Files) > 0 {
+			for _, f := range fls.Files {
+				fmt.Printf("[[[ Processing %s (%s) ]]]\n", f.Name, f.Id)
 
-			// payload buffer is full
-			// if len(payloads) == cap(payloads) {
-			// 	// send payload
-			// }
-
-			// // payload is full
-			// if len(pyld.Items) == cap(pyld.Items) {
-
-			// 	payloads = append(payloads, pyld)
-
-			// 	// empty out payload items
-			// 	pyld.Items = make([]Item, 0, 100)
-			// }
-
-			// add item to payload
-			// pyld.Items = append(pyld.Items, iv)
-
-			fmt.Printf("\t\t\"LocationCode\":\"%s\"\n", iv.LocationCode)
-			fmt.Printf("\t\t\"Quantity\":%d\n", iv.Quantity)
-			fmt.Printf("\t\t\"Sku\":\"%s\"\n", iv.Sku)
-			fmt.Printf("\t\t\"WarehouseId\":\"%d\"\n", iv.WarehouseID)
+				wg.Add(1)
+				go chunkToPayloads(*f)
+			}
+		} else {
+			fmt.Println("No files found.")
 		}
 	}
 
-	fmt.Printf("Tenant:%s User:%s\n", sku.t.TenantToken, sku.t.UserToken)
 }
 
-func sendPayloads(pylds *[]Payload) {
-	// iterate through all 10 payloads
-	for _, p := range *pylds {
-		go sku.Request("setQuantities", &p)
-	}
-}
-
-// Request sends a POST request using a SKU connection
-// it attempts to send a payload
+// chunkToPayloads downloads a file
+// fitting it into 100-chuck payloads
 //
-func (sc *SkuConn) Request(fn string, pyld *Payload) {
-	req, err := http.NewRequest("POST", "https://app.skuvault.com/api/"+fn, struct2JSON(*pyld))
-	if err != nil {
-		log.Fatalf("Unable to obtain SKUVault request: %v", err)
-	}
-	req.Header.Add("accept", "application/json")
-	req.Header.Add("content-type", "application/json")
+func chunkToPayloads(f drive.File) {
+	defer wg.Done()
 
-	// http client based on request initialized
-	res, err := sc.c.Do(req)
+	// fmt.Println(`[[[ Chunk to payloads: BEGIN ]]]`)
+
+	// grabs http request for one of the json files
+	res, err := drv.Files.Get(f.Id).Download()
 	if err != nil {
-		log.Fatalf("Unable to get SKUVault response: %v", err)
+		log.Fatalf("Unable to download file: %v", err)
 	}
 	defer res.Body.Close()
+
+	plCap := 100
+
+	// 100-item capacity payload
+	pl := Payload{make([]Item, 0, plCap), toks.TenantToken, toks.UserToken}
+
+	// the entire JSON file structure
+	vsd := map[string]map[string]Item{}
+	// fmt.Println(`[[[ Decode JSON: PRE ]]]`)
+	json.NewDecoder(res.Body).Decode(&vsd)
+	// fmt.Println(`[[[ Decode JSON: POST ]]]`)
+	for _, v := range vsd {
+		// fmt.Printf("%s:\n", k)
+
+		for _, iv := range v {
+			// this is one payload item
+
+			// fmt.Printf("\t%s:\n", ik)
+
+			// payload is full
+			if len(pl.Items) == cap(pl.Items) {
+				// fmt.Println(`[[[ Full payload: BEGIN ]]]`)
+				// forward payload into buffered channel
+				wg.Add(1)
+				plBufCh <- pl
+				// reset payload
+				pl = Payload{make([]Item, 0, plCap), pl.TenantToken, pl.UserToken}
+				// fmt.Println(`[[[ Full payload: END ]]]`)
+			}
+
+			// add item to payload
+			pl.Items = append(pl.Items, iv)
+
+			// fmt.Printf("\t\t\"LocationCode\":\"%s\"\n", iv.LocationCode)
+			// fmt.Printf("\t\t\"Quantity\":%d\n", iv.Quantity)
+			// fmt.Printf("\t\t\"Sku\":\"%s\"\n", iv.Sku)
+			// fmt.Printf("\t\t\"WarehouseId\":\"%d\"\n", iv.WarehouseID)
+		}
+
+		// payload is partially full
+		if len(pl.Items) != 0 {
+			// fmt.Println(`[[[ Full payload: BEGIN ]]]`)
+			// forward payload into buffered channel
+			wg.Add(1)
+			plBufCh <- pl
+		}
+	}
+
+	// we are done reading; trash it
+	err = drv.Files.Delete(f.Id).Do()
+	if err != nil {
+		log.Fatalf("Unable to delete file: %v", err)
+	}
+
+	// fmt.Printf("Tenant:%s User:%s\n", toks.TenantToken, toks.UserToken)
+	// fmt.Println(`[[[ Chunk to payloads: END ]]]`)
+}
+
+// writeVault writes the intercepted json files out
+// to SKUVault via its REST api
+//
+func writeVault(pl Payload) {
+	defer wg.Done()
+
+	// fmt.Println(`[[[ Write to SKUVault: BEGIN ]]]`)
+
+	// b, er := ioutil.ReadAll(struct2JSON(pl))
+	// if er != nil {
+	// 	log.Fatalf(`Unable to read payload: %v`, er)
+	// }
+	// fmt.Println(string(b))
+
+	fmt.Printf("[[[ Uploading (%d/%d) ]]]", len(pl.Items), cap(pl.Items))
+	res, err := vaultRequest(`inventory/setItemQuantities`, struct2JSON(pl))
+	if err != nil {
+		log.Fatalf(`Unable to set quantities in SKUVault: %v`, err)
+	}
+	defer res.Body.Close()
+
+	fmt.Printf(" (Status: %d)\n", res.StatusCode)
+	// fmt.Println(`[[[ Write to SKUVault: END ]]]`)
 }
